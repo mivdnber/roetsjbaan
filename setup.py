145f40690b98a27dcfda3d5b2d4e3bee3f1e6c69
Michiel Van den Berghe <michiel.vdb@gmail.com>
2015-04-23 17:34:49 +0200

    Fix requirement file path at setup time

--- setup.py
+++ setup.py
@@ -1,14 +1,15 @@
 from setuptools import setup
+from os.path import join, dirname
 
-with open('requirements.txt') as f:
+with open(join(dirname(__file__), 'requirements.txt')) as f:
     requirements = list(f)
 
 setup(
     name='roetsjbaan',
-    version='0.1',
+    version='0.1.1',
     description='A minimal, lightweight migration system',
     url='https://github.com/mivdnber/roetsjbaan',
-    download_url='https://github.com/mivdnber/roetsjbaan/tarball/0.1',
+    download_url='https://github.com/mivdnber/roetsjbaan/tarball/0.1.1',
     author='Michiel Van den Berghe',
     author_email='michiel.vdb@gmail.com',
     license='MIT',
