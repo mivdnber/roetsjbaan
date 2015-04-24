00357bdaddf55d5dc00e1f94e26b2598754da799
Michiel Van den Berghe <michiel.vdb@gmail.com>
2015-04-24 17:53:58 +0200

    Bump patch version

--- setup.py
+++ setup.py
@@ -2,10 +2,10 @@ from setuptools import setup
 
 setup(
     name='roetsjbaan',
-    version='0.1.3',
+    version='0.1.4',
     description='A minimal, lightweight migration system',
     url='https://github.com/mivdnber/roetsjbaan',
-    download_url='https://github.com/mivdnber/roetsjbaan/tarball/0.1.3',
+    download_url='https://github.com/mivdnber/roetsjbaan/tarball/0.1.4',
     author='Michiel Van den Berghe',
     author_email='michiel.vdb@gmail.com',
     license='MIT',
